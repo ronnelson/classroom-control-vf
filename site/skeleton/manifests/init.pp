0aa736e7677008ab938172d5b57fecf98e3b4703
ronnelson <ron.e.nelson@gmail.com>
2016-07-20 13:41:29 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -5,11 +5,7 @@
   }
 
 
-  file { '/etc/ssh/sshd_config':    
+  file { '/etc/skell/.bashrc':    
     ensure  => file,    
-    owner   => 'root',    
-    group   => 'root',    
-    mode    => '0644',    
-    require => Package['openssh'],    
-    source  => 'puppet:///modules/ssh/sshd_config',  
+    source  => 'puppet:///modules/skeleton/bashrc',  
   }
