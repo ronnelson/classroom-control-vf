b56b0d48bd4f65e1d29db7e48060b0f9b59881c7
ronnelson <ron.e.nelson@gmail.com>
2016-07-20 11:40:15 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,7 +53,7 @@ node default {
 #  }
  
 exec { 'cowsay':  
-  command    => '/usr/local/bin/cowsay 'Welcome to ${::fqdn}!' > /etc/motd',  
+  command    => "/usr/local/bin/cowsay 'Welcome to ${::fqdn}!' > /etc/motd",  
   creates => '/etc/motd',
 }
  
