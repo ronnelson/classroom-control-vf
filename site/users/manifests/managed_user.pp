a4bdb5fe5e9f05e7e6a07906508a4f580a01a123
Ron Nelson <ron.e.nelson@gmail.com>
2016-07-21 22:42:39 +0000

    15.1 stuff

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,20 @@
+define users::managed_user (  
+  $group = $title, 
+  $shell = '/bin/bash',
+  $create_ssh_dir = true,
+) {  
+  user { $title:    
+    ensure => present,  
+  }  
+  file { "/home/${title}":    
+    ensure => directory,    
+    owner  => $title,    
+    group  => $group,  
+  } 
+  file { "/home/${title}/.ssh":    
+    ensure => directory,    
+    owner  => $title,    
+    group  => $group,  
+    mode   => '0700',
+  } 
+}
